14b83f7a415c4096afc473b963d38af2699f07a7
Naisalie <naisaliew@gmail.com>
2023-06-08 16:20:00 -0400

    Add event listeners for sound

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ window.addEventListener("keydown", (e) => {
   let thisKey = document.querySelector(`.key[data-key=${e.key}]`);
   let thisSound = document.querySelector(`audio[data-key=${e.key}]`);
   if (thisKey) {
-    thisSound.play();
-    thisKey.classList.add("playing");
-    setTimeout(() => {
+    thisSound.addEventListener("play", () => {
+      thisKey.classList.add("playing");
+    });
+    thisSound.addEventListener("ended", () => {
       thisKey.classList.remove("playing");
-    }, 400);
+    });
+    thisSound.play();
   }
 });
