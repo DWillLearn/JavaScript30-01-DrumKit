ecd5133bd88d7f7e8b4e0cd6d8778420d92248af
Naisalie <naisaliew@gmail.com>
2023-06-08 15:09:21 -0400

    Add playing animation

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const hitKey = (key) => {
   let thisKey = document.querySelector(`.key[data-key =${key}]`);
   let thisSound = document.querySelector(`audio[data-key =${key}]`);
   if (thisKey) {
+    thisKey.classList.add("playing");
     thisSound.play();
+    setTimeout(() => thisKey.classList.remove("playing"), 500);
   }
 };
 
