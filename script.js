f7305d0f5b1628c0cfed9cfe30188b86960e111a
Naisalie <naisaliew@gmail.com>
2023-06-08 16:48:57 -0400

    Run event listener at end of longest transition

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 window.addEventListener("keydown", (e) => {
   let thisKey = document.querySelector(`.key[data-key=${e.key}]`);
   let thisSound = document.querySelector(`audio[data-key=${e.key}]`);
+
   if (!thisSound) return;
+
   thisSound.currentTime = 0;
   thisSound.play();
   thisKey.classList.add("playing");
-  thisKey.addEventListener("transitionend", () => {
+
+  thisKey.addEventListener("transitionend", (e) => {
+    if (e.propertyName !== "transform") return;
     thisKey.classList.remove("playing");
   });
 });
