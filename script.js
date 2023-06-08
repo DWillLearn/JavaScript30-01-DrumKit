14aca9c23bad8b689f5980b66b26a494065fc61f
Naisalie <naisaliew@gmail.com>
2023-06-08 14:56:34 -0400

    Add play command to hitKey func, remove comments

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ const hitKey = (key) => {
   let thisKey = document.querySelector(`.key[data-key =${key}]`);
   let thisSound = document.querySelector(`audio[data-key =${key}]`);
   if (thisKey) {
-    console.log(thisKey, thisSound);
+    thisSound.play();
   }
 };
 
 window.addEventListener("keydown", (e) => {
   hitKey(e.key);
 });
-//When key is input into a  function as an argument, the argument with a corresponding data-key is also hit
-//A is used as an argument for hitKey
