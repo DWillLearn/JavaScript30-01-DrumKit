d95f24a32a29178fa65f0d8751161da50f54d698
Naisalie <naisaliew@gmail.com>
2023-06-08 16:33:25 -0400

    Fix sound and animation timing

--- script.js
+++ script.js
@@ -3,12 +3,9 @@ window.addEventListener("keydown", (e) => {
   let thisKey = document.querySelector(`.key[data-key=${e.key}]`);
   let thisSound = document.querySelector(`audio[data-key=${e.key}]`);
   if (thisKey) {
-    thisSound.addEventListener("play", () => {
-      thisKey.classList.add("playing");
-    });
-    thisSound.addEventListener("ended", () => {
-      thisKey.classList.remove("playing");
-    });
+    thisSound.currentTime = 0;
     thisSound.play();
+    thisKey.classList.add("playing");
+    setTimeout(() => thisKey.classList.remove("playing"), 100);
   }
 });
