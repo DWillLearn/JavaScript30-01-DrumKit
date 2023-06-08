ed9b0ffb04e97372fee9364922600d049fda2665
Naisalie <naisaliew@gmail.com>
2023-06-08 16:39:23 -0400

    Create a better if statement for nonexistent sound

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 window.addEventListener("keydown", (e) => {
-  e.preventDefault();
   let thisKey = document.querySelector(`.key[data-key=${e.key}]`);
   let thisSound = document.querySelector(`audio[data-key=${e.key}]`);
-  if (thisKey) {
-    thisSound.currentTime = 0;
-    thisSound.play();
-    thisKey.classList.add("playing");
-    setTimeout(() => thisKey.classList.remove("playing"), 100);
-  }
+  if (!thisSound) return;
+  thisSound.currentTime = 0;
+  thisSound.play();
+  thisKey.classList.add("playing");
+  setTimeout(() => thisKey.classList.remove("playing"), 100);
 });
