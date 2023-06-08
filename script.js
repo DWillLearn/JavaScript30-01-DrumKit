1b801173cb1ad8d540c2c28bd7fc7d018241d523
Naisalie <naisaliew@gmail.com>
2023-06-08 15:31:18 -0400

    Merge function and event listener

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const hitKey = (key) => {
-  let thisKey = document.querySelector(`.key[data-key =${key}]`);
-  let thisSound = document.querySelector(`audio[data-key =${key}]`);
-  if (thisKey) {
-    thisKey.classList.add("playing");
-    thisSound.play();
-    setTimeout(() => thisKey.classList.remove("playing"), 500);
-  }
-};
-
 window.addEventListener("keydown", (e) => {
-  hitKey(e.key);
+  e.preventDefault();
+  let thisKey = document.querySelector(`.key[data-key=${e.key}]`);
+  let thisSound = document.querySelector(`audio[data-key=${e.key}]`);
+  if (thisKey) {
+    thisSound.play();
+    thisKey.classList.add("playing");
+    setTimeout(() => {
+      thisKey.classList.remove("playing");
+    }, 400);
+  }
 });
